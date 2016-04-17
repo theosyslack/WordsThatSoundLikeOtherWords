33f0101913235ef137add0dc19df9ebd1f188abf
Theo Syslack <theosyslack@gmail.com>
2016-04-17 10:34:22 -0500

    renabled the whole saving thing

--- app/Http/Controllers/RhymeController.php
+++ app/Http/Controllers/RhymeController.php
@@ -13,26 +13,26 @@ class RhymeController extends Controller
 {
     public function save(Request $request, Phrase $phrase){
         //
-        // $validation = $this->validate($request, [
-        //     'phrase' => 'bail|required',
-        //     'rhyme' => 'bail|required'
-        // ]);
-        //
-        // $timestamp = [];
-        // $submitter = [];
-        //
-        // if(!empty($request->timestamp)){
-        //   $timestamp = Timestamp::firstOrCreate($request->timestamp);
-        // }
-        //
-        // if(!empty($request->submitter)){
-        //   $submitter = Submitter::firstOrCreate($request->submitter);
-        // }
-        //
-        // $phrase = Phrase::firstOrCreate(['text' => $request->phrase]);
-        // $rhyme = Phrase::firstOrCreate(['text' => $request->rhyme]);
-        //
-        // $phraseWithRhymes = $phrase->associateRhyme($rhyme, $timestamp, $submitter);
+        $validation = $this->validate($request, [
+            'phrase' => 'bail|required',
+            'rhyme' => 'bail|required'
+        ]);
+
+        $timestamp = [];
+        $submitter = [];
+
+        if(!empty($request->timestamp)){
+          $timestamp = Timestamp::firstOrCreate($request->timestamp);
+        }
+
+        if(!empty($request->submitter)){
+          $submitter = Submitter::firstOrCreate($request->submitter);
+        }
+
+        $phrase = Phrase::firstOrCreate(['text' => $request->phrase]);
+        $rhyme = Phrase::firstOrCreate(['text' => $request->rhyme]);
+
+        $phraseWithRhymes = $phrase->associateRhyme($rhyme, $timestamp, $submitter);
     }
 
 
